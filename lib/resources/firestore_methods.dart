bc80758d7620cae25f1ac3ac4980cea4e1f791db
yarachidiac <88508520+yarachidiac@users.noreply.github.com>
2023-05-11 17:05:11 +0300

    test

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -5,5 +5,5 @@ import 'package:project_live_streaming/resources/storage_methods.dart';
 class FirestoreMethods{
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final StorageMehtods _storageMethods = StorageMehtods();
-
+//test
 }
