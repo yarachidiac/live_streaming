dca450646bed65afa2c5cf847d6d70d396c4c6f0
SABBIDO007 <sabbidinewalid12345@gmail.com>
2023-05-11 17:10:16 +0300

    startlive

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -15,5 +15,56 @@ class FirestoreMethods{
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final StorageMehtods _storageMethods = StorageMehtods();
 
+  Future<String> startLiveStream(BuildContext context, String title, Uint8List? image) async{
+    final user = Provider.of<UserProvider>(context, listen: false);
+    String channelId = '';
+    try{
+      if(title.isNotEmpty && image != null){
+        if(!((await _firestore.collection('livestream').doc('${user.user.uid}${user.user.username}').get()).exists)){
+
+          String thumbnailUrl = await _storageMethods.uploadImageStorage('livestream-thumbnails', image, user.user.uid);
+
+          //channelId = uidusername
+          channelId = '${user.user.uid}${user.user.username}';
+
+          LiveStream liveStream = LiveStream(title: title, image: thumbnailUrl, uid: user.user.uid, username: user.user.username, startedAt: DateTime.now(), viewers: 0, channelId: channelId);
+
+          _firestore.collection('livestream').doc(channelId).set(liveStream.toMap());
+        }else{
+          showSnackBar(context, 'Two Livestreams cannot start at the same time.');
+        }
+      }else{
+        showSnackBar(context, 'Please enter all the fields');
+      }
+
+    } on FirebaseException catch(e){
+      showSnackBar(context, e.message!);
+    }
+    return channelId;
+  }
+
+
+  Future<void> endLiveStream(String channelId) async{
+    try{
+      QuerySnapshot snap =await _firestore
+          .collection('livestream')
+          .doc(channelId)
+          .collection('comments')
+          .get();
+      for(int i = 0; i<snap.docs.length; i++){
+        await _firestore
+            .collection('livestream')
+            .doc(channelId)
+            .collection('comments')
+            .doc(
+          ((snap.docs[i].data()! as dynamic)['commentId']),
+        )
+            .delete();
+      }
+      await _firestore.collection('livestream').doc(channelId).delete();
+    }catch(e){
+      debugPrint(e.toString());
+    }
+    }
 
 }
