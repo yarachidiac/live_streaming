5374c1e2d6e2eb1205355f2240ad5fbfbe0ed9bb
SABBIDO7 <68105510+SABBIDO7@users.noreply.github.com>
2023-05-13 00:03:20 +0300

    feed_update

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:project_live_streaming/models/livestream.dart';
+import 'package:project_live_streaming/screens/broadcast_screen.dart';
 
+import '../resources/firestore_methods.dart';
 import '../widgets/loading_indicator.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class FeedScreen extends StatefulWidget {
   const FeedScreen({Key? key}) : super(key: key);
@@ -17,67 +20,83 @@ class _FeedScreenState extends State<FeedScreen> {
     final size = MediaQuery.of(context).size;
     return SafeArea(child: Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0).copyWith(
-        top: 10
+          top: 10
       ),
       child: Column(
         children: [
           const Text('Live Users', style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 22
+              fontWeight: FontWeight.bold,
+              fontSize: 22
           ),
           ),
           SizedBox(height: size.height*0.03,),
 
           StreamBuilder<dynamic>(
-              stream: FirebaseFirestore.instance
-                  .collection('livestream')
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if(snapshot.connectionState == ConnectionState.waiting){
-                  return const LoadingIndicator();
-                }
-                return ListView.builder(
-                  itemCount: snapshot.data.docs.length,
-                  itemBuilder: (context, index) {
-                    LiveStream post = LiveStream.fromMap(
-                        snapshot.data.docs[index].data);
+            stream: FirebaseFirestore.instance
+                .collection('livestream')
+                .snapshots(),
+            builder: (context, snapshot) {
+              if(snapshot.connectionState == ConnectionState.waiting){
+                return const LoadingIndicator();
+              }
+              return Expanded(
+                child: ListView.builder(
+                    itemCount: snapshot.data.docs.length,
+                    itemBuilder: (context, index) {
+                      LiveStream post = LiveStream.fromMap(
+                          snapshot.data.docs[index].data());
 
-                    return InkWell(
-                      onTap: (){},
-                      child: Container(
-                        height: size.height * 0.1,
-                        margin: const EdgeInsets.symmetric(vertical: 10, ),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            AspectRatio(aspectRatio: 16/9, 
-                              child: Image.network(post.image),
-                            ),
-                            SizedBox(width: 10,),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      return InkWell(
+                          onTap: () async{
+                            await FirestoreMethods().updateViewCount(post.channelId, true);
+                            Navigator.of(context).push(
+                              MaterialPageRoute(builder: (context) => BroadcastScreen(
+                                  isBroadcaster: false,
+                                  channelId: post.channelId)
+                              ),
+                            );
+
+                          },
+                          child: Container(
+                            height: size.height * 0.1,
+                            margin: const EdgeInsets.symmetric(vertical: 10, ),
+                            child: Row(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(post.username, style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20
-                                ),),
-                                Text(post.title, style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20
-                                ),),
-                                Text('${post.viewers}watching'),
-                                Text('Started' ),
-                              ]
-                            )
-                          ],
-                        ),
-                      )
+                                AspectRatio(aspectRatio: 16/9,
+                                  child: Image.network(post.image),
+                                ),
+                                SizedBox(width: 10,),
+                                Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(post.username, style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20
+                                      ),),
+                                      Text(post.title, style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20
+                                      ),),
+                                      Text('${post.viewers}watching'),
+                                      Text('Started ${timeago.format(post.startedAt.toDate())}' ,
+                                      ),
+                                    ]
+                                ),
+                                IconButton(
+                                  onPressed: (){},
+                                  icon: const Icon(Icons.more_vert)
+                                  ,)
+                              ],
+                            ),
+                          )
 
-                    );
-                  }
-                );
-              },)
+                      );
+                    }
+                ),
+              );
+            },)
         ],
 
       ),
