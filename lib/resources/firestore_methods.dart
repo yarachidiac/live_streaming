288cf740b491250966a2db6190e525e4b197cb91
yarachidiac <88508520+yarachidiac@users.noreply.github.com>
2023-05-15 12:36:42 +0300

    profileupdate

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -9,6 +9,7 @@ import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 
 import '../models/livestream.dart';
+import '../models/post.dart';
 import '../models/user.dart';
 import '../utils/utils.dart';
 
@@ -166,6 +167,32 @@ class FirestoreMethods{
     } catch (error) {
       print('Error unfollowing broadcaster: $error');
     }
+  }
+
+  Future<String> uploadPost(String description, Uint8List file, String uid,
+      String username, String profImage) async {
+    // asking uid here because we dont want to make extra calls to firebase auth when we can just get from our state management
+    String res = "Some error occurred";
+    try {
+      String photoUrl =
+      await StorageMehtods().uploadImageToStorage('posts', file, true,uid);
+      String postId = const Uuid().v1(); // creates unique id based on time
+      Post post = Post(
+        description: description,
+        uid: uid,
+        username: username,
+        likes: [],
+        postId: postId,
+        datePublished: DateTime.now(),
+        postUrl: photoUrl,
+        profImage: profImage,
+      );
+      _firestore.collection('posts').doc(postId).set(post.toJson());
+      res = "success";
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
   }
 
 
--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
 class StorageMehtods{
   FirebaseStorage _storage = FirebaseStorage.instance;
@@ -16,6 +17,27 @@ class StorageMehtods{
     String downloadUrl = await snapshot.ref.getDownloadURL();
     return downloadUrl;
   }
+
+  Future<String> uploadImageToStorage(String childName, Uint8List file, bool isPost, String uid) async {
+    // creating location to our firebase storage
+    String id = const Uuid().v1();
+    Reference ref =
+    _storage.ref().child(childName).child(uid);
+
+
+    ref = ref.child(id);
+
+
+    // putting in uint8list format -> Upload task like a future but not future
+    UploadTask uploadTask = ref.putData(
+        file
+    );
+
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
 
 
 
